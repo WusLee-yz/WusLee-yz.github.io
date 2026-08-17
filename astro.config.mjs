8b3517a57d074eb1323767d6ac4a8ae2d3c5d6d8
WusLee-yz <201520333+WusLee-yz@users.noreply.github.com>
2026-08-18 06:10:07 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://wuslee-yz.github.io",
+	site: "wuslee-yz.top",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
